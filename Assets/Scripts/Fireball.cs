e7a68370c88ddbd55e1df955eb2f709fcd42f44e
lunariz <Of3CCGPa8Zr2T%9G2&j5>
2019-03-05 13:45:12 +0000

    Initial Project

---
+++ Assets/Scripts/Fireball.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fireball : MonoBehaviour
+{
+	private Vector3 direction;
+	private float speed;
+
+	public void Initialize(Vector3 direction, float speed)
+	{
+		this.direction = direction;
+		this.speed = speed;
+	}
+	
+	public void Update()
+	{
+		transform.position += direction.normalized * speed;
+	}
+}
