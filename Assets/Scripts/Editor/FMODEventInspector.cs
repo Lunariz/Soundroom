9fbba1c59263c330426cea034ad4d0885cce468b
lunariz <Of3CCGPa8Zr2T%9G2&j5>
2019-03-11 13:52:28 +0000

    Fixed FMOD Bank with added sound, adding events and parameters

---
+++ Assets/Scripts/Editor/FMODEventInspector.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(FMODEvent))]
+public class FMODEventInspector : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		FMODEvent FMODEvent = (FMODEvent) target;
+
+		if (GUILayout.Button("Add Parameter"))
+		{
+			GameObject newParamGO = new GameObject("NewParameter");
+			FMODParameter newParam = newParamGO.AddComponent<FMODParameter>();
+			newParamGO.transform.parent = FMODEvent.transform;
+			Selection.activeGameObject = newParamGO;
+		}
+
+		FMODParameter[] parameters = FMODEvent.GetComponentsInChildren<FMODParameter>();
+		FMODEvent.Parameters = parameters;
+
+		base.OnInspectorGUI();
+	}
+}
---
+++ Assets/Scripts/FMODEvent.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using FMODUnity;
+using System.Collections;
+
+[RequireComponent(typeof(FMODUnity.StudioEventEmitter))]
+public class FMODEvent : MonoBehaviour
+{
+	public FMODParameter[] Parameters;
+
+	private StudioEventEmitter m_event;
+
+	public void Awake()
+	{
+		m_event = GetComponent<StudioEventEmitter>();
+	}
+}
