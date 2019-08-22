313c10e99424b395b8c30350570311786d249ed2
lunariz <Of3CCGPa8Zr2T%9G2&j5>
2019-08-22 15:06:35 +0100

    Added hands

---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class Hand : MonoBehaviour
+{
+	public XRNode NodeType;
+
+	public void Start()
+	{
+		//XRDevice.SetTrackingSpaceType(TrackingSpaceType.RoomScale);
+	}
+
+	public void Update()
+	{
+		transform.localPosition = InputTracking.GetLocalPosition(NodeType);
+		transform.localRotation = InputTracking.GetLocalRotation(NodeType);
+	}
+}
