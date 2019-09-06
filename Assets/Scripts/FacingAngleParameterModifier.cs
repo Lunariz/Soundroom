8e3fda2d3600bfc4c2cd9fd77b58ab2eae1299fd
lunariz <Of3CCGPa8Zr2T%9G2&j5>
2019-09-06 17:04:10 +0100

    Added the ability to track hand pointing direction using the FacingAngleParameterModifier

--- Assets/Scripts/FacingAngleParameterModifier.cs
+++ Assets/Scripts/FacingAngleParameterModifier.cs
@@ -5,11 +5,12 @@ using System.Collections.Generic;
 public class FacingAngleParameterModifier : ParameterModifier
 {
 	public float CurrentParameterValue;
+	public TrackObject SourceObject;
 	public TrackAxis RotationAxis;
 
 	public void Update()
 	{
-		Vector3 playerRotation = PlayerManager.Instance.Head.transform.rotation.eulerAngles;
+		Vector3 playerRotation = TrackObjectUtility.GetObject(SourceObject).transform.rotation.eulerAngles;
 		Vector3 modifiedPlayerRotation = new Vector3(
 			Clamp180(playerRotation.y),
 			-Clamp180(playerRotation.x),
